b87a9284edbdb2d32075ea7cf36d2f6945008238
cjmunozy <cjmunoz@espol.edu.ec>
2023-12-08 00:18:00 -0500

    Agregando código base para el patrón de diseño Chain of Responsibility

---
+++ 3) Chain of Responsibility/ResponsibilityChain/src/main/java/ec/edu/espol/responsibilitychain/ResponsibilityChain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.responsibilitychain;
+
+/**
+ *
+ * @author crisj
+ */
+public class ResponsibilityChain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
